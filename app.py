0b1ab3969c198168302ba8fa0b6231507c90c9c9
aybukebetulyarim <yarim.aybukebetul@student.atilim.edu.tr>
2021-09-06 17:13:30 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+from tables import LibraryTable, LoginfoTable
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/turkai/Desktop/library/librarydatabase.db'
+db = SQLAlchemy(app)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/books")
+def listofBooks():
+    book = LibraryTable(book_name="kitap ismi",edition_year=1999,author="yazar ismi",owner_name="sahibi",category="aaa",translator="çevirmen")
+    db.session.add(book)
+    db.session.commit()
+    return render_template("book.html",book_ht=book)
---
+++ tables.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/turkai/Desktop/library/librarydatabase.db'
+db = SQLAlchemy(app) 
+
+class LibraryTable(db.Model):
+    __tablename__= "libraryTable"
+    book_id      = db.Column(db.Integer, primary_key = True)
+    book_name    = db.Column(db.String)
+    edition_year = db.Column(db.String)
+    author       = db.Column(db.String)
+    owner_name   = db.Column(db.String)
+    category     = db.Column(db.String)
+    translator   = db.Column(db.String)
+    date_time    = db.Column(db.String)
+    log          = db.relationship('LoginfoTable', backref='library', lazy=True)
+
+    def __init__(self,book_name:str,edition_year:int,author:str,owner_name:str,category:str,translator:str):
+        self.book_name    = book_name
+        self.edition_year = edition_year
+        self.author       = author
+        self.owner_name   = owner_name
+        self.category     = category
+        self.translator   = translator
+        self.date_time    = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+class LoginfoTable(db.Model):
+    __tablename__ = "logTable"
+    book_id_log   = db.Column(db.Integer, primary_key=True)
+    date_time     = db.Column(db.String)
+    process       = db.Column(db.String)
+    library_id    = db.Column(db.Integer, db.ForeignKey('libraryTable.book_id'))
+    
+    def __init__(self,process:str,lib_id:int):
+        self.process   = process
+        self.date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.lib_id    = lib_id
+
+db.create_all()
+
