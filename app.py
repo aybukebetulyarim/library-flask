49e669d870475fca466a810ba07adf0c31c97bfe
aybukebetulyarim <yarim.aybukebetul@student.atilim.edu.tr>
2021-09-09 17:23:16 +0300

    last commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 from tables import LibraryTable, LoginfoTable
@@ -7,6 +7,7 @@ from tables import LibraryTable, LoginfoTable
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/turkai/Desktop/library/librarydatabase.db'
+
 db = SQLAlchemy(app)
 
 
@@ -14,9 +15,23 @@ db = SQLAlchemy(app)
 def index():
     return render_template("index.html")
 
-@app.route("/books")
+@app.route("/signup", methods=["GET","POST"])
+def signup():
+    return render_template("signup.html")
+
+@app.route("/books", methods=["POST"])
 def listofBooks():
     book = LibraryTable(book_name="kitap ismi",edition_year=1999,author="yazar ismi",owner_name="sahibi",category="aaa",translator="çevirmen")
-    db.session.add(book)
+    # db.session.add(book)
+    books = db.session.query(LibraryTable).all()
     db.session.commit()
-    return render_template("book.html",book_ht=book)
+    return render_template("books.html",books=books)
+
+@app.route("/addBook", methods=["POST","GET"])
+def addBook():
+    return render_template("addBook.html")
+
+if __name__ == '__main__':
+    app.run(host='192.168.1.134',debug=True,port=5000)
+
+ 
--- tables.py
+++ tables.py
@@ -6,6 +6,7 @@ import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/turkai/Desktop/library/librarydatabase.db'
+
 db = SQLAlchemy(app) 
 
 class LibraryTable(db.Model):
